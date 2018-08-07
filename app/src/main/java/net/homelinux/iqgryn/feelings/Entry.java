4d1b0a3e3ebb315b7cd48a74bd7799fb0ee8432a
Kevin Lyles <kevinlyles@gmail.com>
2018-08-06 19:46:09 -0500

    Initial commit

---
+++ app/src/main/java/net/homelinux/iqgryn/feelings/Entry.java
@@ -0,0 +1,13 @@
+package net.homelinux.iqgryn.feelings;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Entry extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_entry);
+	}
+}
